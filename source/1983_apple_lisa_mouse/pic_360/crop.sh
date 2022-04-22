2442d42dab98dade606f6197ddbe16d5c567b01d
Dima <dmitriykostiuk@gmail.com>
2022-04-22 22:56:44 +0300

    add lisa mouse 365-view

---
+++ source/1983_apple_lisa_mouse/pic_360/crop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p ./result/img
+y=1
+for filename in ./*.jpg; do
+  echo $filename
+  newfilename=$(printf "%02d" $y)
+  y=$((y+1))
+  echo $newfilename
+  convert $filename -gravity Center -crop 3400x2000+100-300 ./result/img/$newfilename.jpg
+#  break
+done
