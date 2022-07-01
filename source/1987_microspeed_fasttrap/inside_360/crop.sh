f4beb2948f423f16e0b4eb2fd96729ff7b36e22e
Dima <dmitriykostiuk@gmail.com>
2022-07-02 01:48:16 +0300

    add fasttrap trackball 360-view

---
+++ source/1987_microspeed_fasttrap/inside_360/crop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p ./result/img
+y=1
+for filename in ./*.jpg; do
+  echo $filename
+  newfilename=$(printf "%02d" $y)
+  y=$((y+1))
+  echo $newfilename
+  convert $filename -gravity Center -crop 4000x2560+50-100 ./result/img/$newfilename.jpg
+#   break
+done
