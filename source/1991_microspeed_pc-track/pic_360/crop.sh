1bed829d4c5fbfa5f2b8beaf1ec751f0cb272eff
Dima <dmitriykostiuk@gmail.com>
2022-07-05 18:36:41 +0300

    Add microspeed pc-track 360-views

---
+++ source/1991_microspeed_pc-track/pic_360/crop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p ./result/img
+y=1
+for filename in ./*.jpg; do
+  echo $filename
+  newfilename=$(printf "%02d" $y)
+  y=$((y+1))
+  echo $newfilename
+  convert $filename -gravity Center -crop 5000x2940-275-310 ./result/img/$newfilename.jpg
+#   break
+done
