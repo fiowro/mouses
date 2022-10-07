9f291d2919ef8edecfa8a88904f608c25c5a5fa0
Dima <dmitriykostiuk@gmail.com>
2022-10-08 01:34:08 +0300

    DEC Vax100 mouse, Microsoft green-eyed mouse, half-ready

---
+++ source/1983_dec_vs10x_ea_mouse/pic_360/crop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p ./result/img
+y=1
+for filename in ./*.jpg; do
+  echo $filename
+  newfilename=$(printf "%02d" $y)
+  y=$((y+1))
+  echo $newfilename
+  convert $filename -gravity Center -crop 2050x1380-100+220 ./result/img/$newfilename.jpg
+#   break
+done
