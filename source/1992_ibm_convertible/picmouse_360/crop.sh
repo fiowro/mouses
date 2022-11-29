f0e8d4f398c6ac48dae465d1b176953b9d6b0332
Dima <dmitriykostiuk@gmail.com>
2022-11-30 01:52:00 +0300

    Kraft and Ibm convetible 360-views

---
+++ source/1992_ibm_convertible/picmouse_360/crop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p ./result/img
+y=1
+for filename in ./*.jpg; do
+  echo $filename
+  newfilename=$(printf "%02d" $y)
+  y=$((y+1))
+  echo $newfilename
+  convert $filename -gravity Center -crop 4800x2600+210+190 ./result/img/$newfilename.jpg
+#   break
+done
