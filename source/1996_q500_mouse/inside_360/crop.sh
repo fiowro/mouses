12e08556aaa5f356b06415e0987064b90d615792
Dima <dmitriykostiuk@gmail.com>
2023-01-04 01:47:55 +0300

    Add Q500 mouse with 360 views

---
+++ source/1996_q500_mouse/inside_360/crop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p ./result/img
+y=1
+for filename in ./*.jpg; do
+  echo $filename
+  newfilename=$(printf "%02d" $y)
+  y=$((y+1))
+  echo $newfilename
+  convert $filename -gravity Center -crop 4000x2000+400-650 ./result/img/$newfilename.jpg
+#   break
+done
