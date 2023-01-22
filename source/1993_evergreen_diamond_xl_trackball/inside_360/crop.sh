0c05ad2d498257465c6ef64cf484836630b3069a
Dima <dmitriykostiuk@gmail.com>
2023-01-22 03:28:53 +0300

    Add Evergreen trackball, make fixes to kraft trackball, add few fpics

---
+++ source/1993_evergreen_diamond_xl_trackball/inside_360/crop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p ./result/img
+y=1
+for filename in ./*.jpg; do
+  echo $filename
+  newfilename=$(printf "%02d" $y)
+  y=$((y+1))
+  echo $newfilename
+  convert $filename -gravity Center -crop 5200x3140+30+50 ./result/img/$newfilename.jpg
+#   break
+done
