3f4cd8600ae3e4c311f54cf75286decfc8ffc890
Dima <dmitriykostiuk@gmail.com>
2023-03-26 12:25:41 +0300

    Add surf mouse with 360 views

---
+++ source/2000_surf_mouse/pic360/crop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p ./result/img
+y=1
+for filename in ./*.jpg; do
+  echo $filename
+  newfilename=$(printf "%02d" $y)
+  y=$((y+1))
+  echo $newfilename
+  convert $filename -gravity Center -crop 5100x3380-260-50 ./result/img/$newfilename.jpg
+#   break
+done
