6b264961bf34b6a012d95de19e9811bacf7026cf
Dima <dmitriykostiuk@gmail.com>
2023-04-08 01:12:04 +0300

    Add IBM ScrollPoint II mouse with 360 views

---
+++ source/2000_ibm_scrollpoint_ii_mouse/pic_360/crop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p ./result/img
+y=1
+for filename in ./*.jpg; do
+  echo $filename
+  newfilename=$(printf "%02d" $y)
+  y=$((y+1))
+  echo $newfilename
+  convert $filename -gravity Center -crop 4040x2300+15-240 ./result/img/$newfilename.jpg
+#   break
+done
