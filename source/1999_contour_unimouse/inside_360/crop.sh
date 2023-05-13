243f44740d6006bf998e6e6f6880e42c032b7b29
Dima <dmitriykostiuk@gmail.com>
2023-05-13 16:17:50 +0300

    fix evergreen trackball pic, add contour unimouse with 360 views

---
+++ source/1999_contour_unimouse/inside_360/crop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p ./result/img
+y=1
+for filename in ./*.jpg; do
+  echo $filename
+  newfilename=$(printf "%02d" $y)
+  y=$((y+1))
+  echo $newfilename
+  convert $filename -gravity Center -crop 5060x3300-260+30 ./result/img/$newfilename.jpg
+#   break
+done
