20c1f14ebe29dc3d4efd5f6c7a663f90a9737157
Dima <dmitriykostiuk@gmail.com>
2023-07-22 23:38:56 +0300

    Add Mouse Systems M4 mouse with 360 views

---
+++ source/1988_mouse_systems_m4/pic_360/crop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p ./result/img
+y=1
+for filename in ./*.jpg; do
+  echo $filename
+  newfilename=$(printf "%02d" $y)
+  y=$((y+1))
+  echo $newfilename
+  convert $filename -gravity Center -crop 4980x2400+480-480 ./result/img/$newfilename.jpg
+#   break
+done
