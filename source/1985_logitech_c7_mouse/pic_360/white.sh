e80cd34782fd0d4760bd61b624971d52cc8520d9
Dima <dmitriykostiuk@gmail.com>
2024-02-20 22:59:16 +0300

    Add Logitech C7 mouse

---
+++ source/1985_logitech_c7_mouse/pic_360/white.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir -p ./result
+y=1
+for filename in ./*.jpg; do
+  echo $filename
+  newfilename=$y
+  y=$((y+1))
+  echo $newfilename
+   ../../../../autowhite -m 1 -p 60 $filename result/$filename
+done
