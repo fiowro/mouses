57b3264f1f99423bec76ec1c8000725d0e479631
Dima <dmitriykostiuk@gmail.com>
2024-04-05 10:37:37 +0300

    add digimouse (sunnyline) 360 views

---
+++ source/1986_sunnyline_digimouse/pic_360/crop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p ./result/img
+y=1
+for filename in ./*.jpg; do
+  echo $filename
+  newfilename=$(printf "%02d" $y)
+  y=$((y+1))
+  echo $newfilename
+  convert $filename -gravity Center -crop 3800x2050-320-600 ./result/img/$newfilename.jpg
+#   break
+done
