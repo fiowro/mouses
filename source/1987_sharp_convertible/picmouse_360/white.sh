f5fabeb01968c6fba6fa68e55923bd5a382d0687
Dima <dmitriykostiuk@gmail.com>
2024-05-12 01:35:02 +0300

    Add sharp convertible and minor fixes to american mouse

---
+++ source/1987_sharp_convertible/picmouse_360/white.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir -p ./result/img
+y=1
+for filename in ./*.JPG; do
+  echo $filename
+  newfilename=$y
+  y=$((y+1))
+  echo $newfilename
+   ../../../../autowhite -m 1 -p 60 $filename result/$filename
+done
